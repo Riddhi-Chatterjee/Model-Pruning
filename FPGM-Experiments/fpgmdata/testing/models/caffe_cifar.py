bf92b2a108a1ee502276324a132bb3b3570c3dcc
Riddhi-Chatterjee <riddhich@rediffmail.com>
2023-07-01 12:51:09 +0530

    Added fpgm experiments

---
+++ FPGM-Experiments/fpgmdata/testing/models/caffe_cifar.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e34f0f0e5a8ea0b2b6262b9edbb3478ac81608b322dac828a697246da384b47
+size 1750
---
+++ FPGM-Experiments/fpgmdata/testing/models/densenet.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:272920858df3b674edbcacdd3a84f5285888a89340a7643288a21bff184dfade
+size 3518
